c5c34d74f3174056cd146ece39abbc22f5c775cd
Joao Ferreira <yferreirajoao@gmail.com>
2025-06-17 17:12:25 -0300

    Removed: ATS in execution

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const codigo = fs.readFileSync('exemplo.ptgl', 'utf-8');
 
 try {
   const ast = parser.parse(codigo);
-  console.log("AST Gerada com Sucesso:", JSON.stringify(ast, null, 2));
-
-  console.log("\n--- Execução ---");
   avaliar(ast);
-  console.log("----------------");
-
 } catch (e) {
   console.error("Ocorreu um erro:", e.message);
 }
